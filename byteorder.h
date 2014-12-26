3e38267a4f2870bb296e807caf967fb2978886fd
antirez <antirez@gmail.com>
2014-12-26 12:44:02 +0100

    Remove stuff created by configure from repo.

--- byteorder.h
+++
@@ -1,8 +0,0 @@
-#ifndef __BYTEORDER_H
-#define __BYTEORDER_H
-
-#ifndef __LITTLE_ENDIAN_BITFIELD
-#define __LITTLE_ENDIAN_BITFIELD
-#endif /* __LITTLE_ENDIAN_BITFIELD */
-
-#endif /* __BYTEORDER_H */
--- systype.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SYSTYPE_H
-#define __SYSTYPE_H
-
-#define OSTYPE_DARWIN
-
-#endif /* SYSTYPE_H */
